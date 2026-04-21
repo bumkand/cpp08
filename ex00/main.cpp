3100a376328410d333490ee7cf9b9fd46ce6eb0f
Jakub Andrasi <jaandras@c2r7s5.42prague.com>
2026-04-21 17:53:50 +0200

    ex00 done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,24 +4,49 @@ int main()
 {
 	std::vector<int> v;
 
-	v.push_back(1);
+	v.push_back(2);
+	v.push_back(12);
 	v.push_back(9);
 	v.push_back(7);
-	v.push_back(2);
-	v.push_back(6);
-	v.push_back(8);
-	v.push_back(4);
+	v.push_back(11);
+	v.push_back(15);
+	v.push_back(5);
+	v.push_back(0);
 
-	try
+	for (int i = 0; i < 16; i++)
 	{
-		std::vector<int>::iterator it = ::easyfind(v, 9);
-		std::cout << *it << std::endl;
+		std::cout << i << " = ";
+
+		try
+		{
+			std::vector<int>::iterator it = ::easyfind(v, i);
+			std::cout << *it << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	
+
+	// std::vector<std::string> c;
+
+	// c.push_back("aaa");
+	// c.push_back("rrr");
+	// c.push_back("ccc");
+	// c.push_back("ddd");
+	// c.push_back("aaa");
+	// c.push_back("jjj");
+
+	// try
+	// {
+	// 	std::vector<std::string>::iterator it = ::easyfind(c, "ccc");
+	// 	std::cout << *it << std::endl;
+	// }
+	// catch (std::exception &e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+
 
 	return 0;
 }
